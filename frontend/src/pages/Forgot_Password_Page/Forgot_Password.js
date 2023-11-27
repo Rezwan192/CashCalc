c4fcccfea034482434be5e2127ff821d21c412a3
Rezwan192 <rez1922001@gmail.com>
2023-11-27 15:23:10 -0500

    Adding home, sign-in, and sign-up pages

---
+++ frontend/src/pages/Forgot_Password_Page/Forgot_Password.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Forgot_Password.css";
+
+function ForgotPassword() {
+  return (
+    <div>
+      <h1>Reset Your Password</h1>
+    </div>
+  );
+}
+
+export default ForgotPassword;
