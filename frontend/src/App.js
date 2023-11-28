66c7aca4c685d1ea37bd061b9b7d2e4885414da3
Rezwan192 <rez1922001@gmail.com>
2023-11-27 23:02:00 -0500

    Adding all routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,12 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
+import Sign_Up from "./pages/Sign_Up_Page/Sign_Up";
+import Sign_In from "./pages/Sign_In_Page/Sign_In";
+import Forgot_Password from "./pages/Forgot_Password_Page/Forgot_Password";
+import BudgetPlanning from "./pages/BudgetPlanning";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Profile from "./pages/Profile_page/Profile";
 import NoPage from "./pages/NoPage";
 import "./App.css";
 
@@ -10,12 +16,12 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-
-        {/* Add more paths to page files
-           <Route path="" element={< X />} />
-          <Route path="" element={< Y />} />
-          <Route path="" element={< Z />} /> */}
-
+        <Route path="sign_up" element={<Sign_Up />} />
+        <Route path="sign_in" element={<Sign_In />} />
+        <Route path="forgot_password" element={<Forgot_Password />} />
+        <Route path="BudgetPlanning" element={<BudgetPlanning />} />
+        <Route path="Dashboard" element={<Dashboard />} />
+        <Route path="Profile" element={<Profile />} />
         <Route path="*" element={<NoPage />} />
       </Route>
     </Routes>
