ebfafa68f278e6c7307f162d3c60591c8d7b5441
Jguinanzaca <Joshua.Guinanzaca16@myhunter.cuny.edu>
2023-12-05 23:26:40 -0500

    merge_attempt

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,8 @@
 const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
-const auth = require("./authentication/auth.js");
-const validate = require("./authentication/validate.js");
 const connectDatabase = require("./mongoDB/connectDatabase.js");
-const testRoutes = require("./mongoDB/testRoutes.js");
 const userRoutes = require("./mongoDB/userRoutes.js");
-const test = require("./mongoDB/testUserCollecion.js");
 
 dotenv.config();
 const app = express();
@@ -14,13 +10,6 @@ const port = process.env.PORT;
 app.use(cors());
 connectDatabase(); //connects server to mongoDB
 
-//const pass = "Hash$2001please"; //plntxt password to test hash function
-//const hash = "$2b$10$bot0aLZDTTAZ3rCfMKtw2u2MK4apmc.W9y7I9X90CTcs3ZS.Cr1w."; //old iteration of hashed pass(will always return true)
-//auth(pass); //function to auth(hashing and salting) in auth.js
-//validate(pass, hash); //function to validate password and hash matching
-//test(); //function to test new schema for cashcalc
-
-app.use("/userPasswords", testRoutes); //test route to displays data(personal for auth testing purposes, will remove later)
 app.use("/cashcalc", userRoutes); //localhost::5000/cashcalc, and corresponding paths in userRoutes.js
 
 app.listen(port, () => {
